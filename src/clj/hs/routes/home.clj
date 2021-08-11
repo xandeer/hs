5f5ad6214e70c05bc27684b575eaba5c0aee88dc
Kevin Du <kkxandeer@gmail.com>
2021-08-11 21:25:06 +0800

    feat: support upload file

--- src/clj/hs/routes/home.clj
+++ src/clj/hs/routes/home.clj
@@ -9,9 +9,6 @@
    [ring.util.response]
    [ring.util.http-response :as response]))
 
-(defn home-page [request]
-  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
-
 (defn about-page [request]
   (layout/render request "about.html"))
 
@@ -22,8 +19,7 @@
     (.startsWith s "~") (cstr/replace-first s "~" (System/getProperty "user.home"))
     :else s))
 
-(def hello-prefix "/hello")
-(def hello-dir "?dir=")
+(def home "/")
 (def static "/static")
 
 (defn root []
@@ -32,37 +28,48 @@
 (defn expand-root []
   (expand-file (root)))
 
+(defn expand-with-root [child]
+  (str (expand-root) child))
+
 (defn ls [folder]
-  (let [path (str (expand-root) folder)
+  (let [path (expand-with-root folder)
         file (io/as-file path)
-        children (.listFiles file)]
+        children (sort-by #(.isFile %) (.listFiles file))]
     (mapv (fn [f]
-            {:href (str (if (.isDirectory f) (str hello-prefix hello-dir)
-                            static)
+            {:href (str (if (.isDirectory f) "?dir=" static)
                         folder "/" (.getName f))
              :name (str (.getName f) (when (.isDirectory f) "/"))})
           children)))
 
-(defn hello-page [request]
+(defn home-page [request]
+  (log/info "req: " request)
   (let [dir (:dir (:params request))
-        items (ls (str dir))]
+        items (ls dir)]
     (layout/render
-     request "hello.html"
-     {:items (if (or (cstr/blank? dir) (.equals (root) dir))
+     request "home.html"
+     {:items (if (or (cstr/blank? dir) (.equals home dir))
                items
-               (into [{:href (str hello-prefix
-                                  (let [parent (subs dir 0 (cstr/last-index-of dir "/"))]
-                                    (when (not (cstr/blank? parent))
-                                      (str hello-dir parent))))
+               (into [{:href (let [parent (subs dir 0 (cstr/last-index-of dir "/"))]
+                               (if (cstr/blank? parent) home
+                                   (str "?dir=" parent)))
                       :name "../"}]
                      items))
       :query dir})))
 
+(defn save-file [req]
+  (let [tmp (:tempfile (:file (:params req)))
+        path (expand-with-root (cstr/replace-first (:uri req) static ""))]
+    (log/info "tmp:" tmp "cus:" path)
+    (do
+      (io/copy tmp (io/file path))
+      {:status 200, :body "ok"})))
+
 (defn home-routes []
   [""
-   {:middleware [middleware/wrap-csrf
+   {:middleware [
+                 ;; middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
-   [hello-prefix {:get hello-page}]
-   ["/static/*" (ring/create-file-handler {:path static :root (expand-root)})]
+   ["/static/*" {:get (ring/create-file-handler {:path static :root (expand-root)})
+                 :post save-file}]
    ["/about" {:get about-page}]])
